dbd3065ac45cb906d19a229a5afad8cf578bb58a
Abraham Gebreselassie <az.gebrese@gmail.com>
2023-10-09 23:13:28 +0000

    "Creating the post method for trips"

---
+++ api/queries/trips.py
@@ -0,0 +1,56 @@
+from pydantic import BaseModel
+from typing import Optional
+from queries.pool import pool
+
+
+class TripIn(BaseModel):
+    planner: str
+    trip_name: str
+    city: str
+    country: str
+    start_date: str
+    end_date: str
+
+
+class TripOut(BaseModel):
+    trip_id: int
+    planner: str
+    trip_name: str
+    city: str
+    country: str
+    start_date: str
+    end_date: str
+
+
+class Error(BaseModel):
+    message: str
+
+
+class CreateTripError(ValueError):
+    pass
+
+
+class TripsRepository:
+    def create_trip(self, trip: TripIn) -> TripOut:
+        with pool.connection() as conn:
+            with conn.cursor() as db:
+                result = db.execute(
+                    """
+                    INSERT INTO trips
+                        (planner, trip_name, city, country, start_date, end_date)
+                    VALUES
+                       (%s, %s, %s, %s, %s, %s)
+                    RETURNING trip_id;
+                    """,
+                    [
+                        trip.planner,
+                        trip.trip_name,
+                        trip.city,
+                        trip.country,
+                        trip.start_date,
+                        trip.end_date,
+                    ],
+                )
+                trip_id = result.fetchone()[0]
+                old_data = trip.dict()
+                return TripOut(trip_id=trip_id, **old_data)
