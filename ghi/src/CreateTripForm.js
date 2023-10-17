4befa668c6ba94b0aa2ff991a18f5be4d8ba412d
Hector  Elias <elias.hector16@gmail.com>
2023-10-17 00:50:29 +0000

    Resolve "Build skeleton for a Create Trip Form Component"

---
+++ ghi/src/CreateTripForm.js
@@ -0,0 +1,15 @@
+import useToken from "@galvanize-inc/jwtdown-for-react";
+import { useState } from "react";
+
+
+function CreateTrip(){
+
+    
+
+    return(
+        <h1>Hello World</h1>
+    );
+
+};
+
+export default CreateTrip;
