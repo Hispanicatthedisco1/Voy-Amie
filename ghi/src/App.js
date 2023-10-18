b110a93eddd4ca125509ff5f473ed7f7e831542a
Miguel Ortiz <migue.ortiz0125@gmail.com>
2023-10-18 01:09:39 +0000

    Create skeleton Profile Page for the F.E.

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Construct from "./Construct.js";
-import ErrorNotification from "./ErrorNotification";
+// import Construct from "./Construct.js";
+// import ErrorNotification from "./ErrorNotification";
 import "./App.css";
 import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
 import SignUpForm from "./SignUpForm.js";
 import LoginForm from "./LogInForm.js";
 import CreateTrip from "./CreateTripForm.js";
+import Nav from "./Nav";
+import UserProfileForm from "./UserProfileForm";
 
 function App() {
   const domain = /https:\/\/[^/]+/;
@@ -16,13 +18,15 @@ function App() {
   return (
     <div className="container">
       <BrowserRouter basename={basename}>
-        <AuthProvider baseUrl={process.env.REACT_APP_API_HOST}>
-          <Routes>
-            <Route exact path="/users" element= {<SignUpForm />}></Route>
-            <Route exact path="/login" element={<LoginForm />}></Route>
-            <Route exact path="/trips" element={ <CreateTrip /> }></Route>
-          </Routes>
-        </AuthProvider>
+        <Nav />
+          <AuthProvider baseUrl={process.env.REACT_APP_API_HOST}>
+            <Routes>
+              <Route exact path="/users" element= {<SignUpForm />}></Route>
+              <Route exact path="/login" element={<LoginForm />}></Route>
+              <Route exact path="/trips" element={ <CreateTrip /> }></Route>
+              <Route exact path="/profile" element={ <UserProfileForm /> }></Route>
+            </Routes>
+          </AuthProvider>
       </BrowserRouter>
     </div>
   );
