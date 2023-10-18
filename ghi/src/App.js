c8bd29dcdc4bae8a2d7269820c47a6ec20497905
Abraham Gebreselassie <az.gebrese@gmail.com>
2023-10-18 22:16:56 +0000

    Resolve "front end deployment"

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,7 +1,4 @@
-import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Construct from "./Construct.js";
-// import ErrorNotification from "./ErrorNotification";
 import "./App.css";
 import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
 import SignUpForm from "./SignUpForm.js";
@@ -14,19 +11,18 @@ function App() {
   const domain = /https:\/\/[^/]+/;
   const basename = process.env.PUBLIC_URL.replace(domain, "");
 
-
   return (
     <div className="container">
       <BrowserRouter basename={basename}>
         <Nav />
-          <AuthProvider baseUrl={process.env.REACT_APP_API_HOST}>
-            <Routes>
-              <Route exact path="/users" element= {<SignUpForm />}></Route>
-              <Route exact path="/login" element={<LoginForm />}></Route>
-              <Route exact path="/trips" element={ <CreateTrip /> }></Route>
-              <Route exact path="/profile" element={ <UserProfileForm /> }></Route>
-            </Routes>
-          </AuthProvider>
+        <AuthProvider baseUrl={process.env.REACT_APP_API_HOST}>
+          <Routes>
+            <Route exact path="/users" element={<SignUpForm />}></Route>
+            <Route exact path="/login" element={<LoginForm />}></Route>
+            <Route exact path="/trips" element={<CreateTrip />}></Route>
+            <Route exact path="/profile" element={<UserProfileForm />}></Route>
+          </Routes>
+        </AuthProvider>
       </BrowserRouter>
     </div>
   );
